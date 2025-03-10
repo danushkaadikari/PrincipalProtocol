3d43283b2dc4a7eff98d71e5749c26378e5d613a
danushkaadikari <danushkaadikari@gmail.com>
2025-03-10 09:52:35 +0700

    Delete supabase/migrations directory

--- supabase/migrations/20250211_fix_property_images.sql
+++
@@ -1,3 +0,0 @@
--- Update property_images column to be JSONB
-ALTER TABLE real_estate_projects DROP COLUMN IF EXISTS property_images;
-ALTER TABLE real_estate_projects ADD COLUMN property_images JSONB DEFAULT NULL;
